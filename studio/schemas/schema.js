161669ddcf5f230f5744ccd0fb42da003a8e5062
OrenBochman <OrenBochman@gmail.com>
2021-10-05 13:09:46 +0300

    reverted breraking changes in web and added movies schema

--- studio/schemas/schema.js
+++ studio/schemas/schema.js
@@ -10,12 +10,23 @@ import category from './documents/category'
 import post from './documents/post'
 import siteSettings from './documents/siteSettings'
 
+import crewMember from './documents/crewMember'
+import castMember from './documents/castMember'
+import movie from './documents/movie'
+import person from './documents/person'
+import screening from './documents/screening'
+
+
 // Object types
 import bodyPortableText from './objects/bodyPortableText'
 import bioPortableText from './objects/bioPortableText'
 import excerptPortableText from './objects/excerptPortableText'
 import mainImage from './objects/mainImage'
 import authorReference from './objects/authorReference'
+import blockContent from './objects/blockContent'
+import plotSummary from './objects/plotSummary'
+import plotSummaries from './objects/plotSummaries'
+
 
 // Then we give our schema to the builder and provide the result to Sanity
 export default createSchema({
@@ -26,17 +37,25 @@ export default createSchema({
   types: schemaTypes.concat([
     // The following are document types which will appear
     // in the studio.
+
     siteSettings,
     post,
     category,
     author,
+    movie,
+    person,
+    screening,
+    // When added to this list, object types can be used as
+    // { type: 'typename' } in other document schemas
+    blockContent,
+    plotSummary,
+    plotSummaries,
+    castMember,
+    crewMember,
     mainImage,
     authorReference,
     bodyPortableText,
     bioPortableText,
-    excerptPortableText
-
-    // When added to this list, object types can be used as
-    // { type: 'typename' } in other document schemas
+    excerptPortableText,
   ])
 })
