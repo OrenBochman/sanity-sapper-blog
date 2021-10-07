bfc1ce5810b185b891a74a0f32722e0f95a0afe7
OrenBochman <OrenBochman@gmail.com>
2021-10-07 15:00:51 +0300

    add slug field~

--- studio/schemas/documents/category.js
+++ studio/schemas/documents/category.js
@@ -7,6 +7,15 @@ export default {
       name: 'title',
       type: 'string',
       title: 'Title'
+    },    
+    {
+      name: 'slug',
+      type: 'slug',
+      title: 'Slug',
+      options: {
+        // add a button to generate slug from the title field
+        source: 'title'
+      }
     },
     {
       name: 'description',
--- studio/schemas/documents/post.js
+++ studio/schemas/documents/post.js
@@ -81,7 +81,35 @@ export default {
       title: 'Body (MarkDown)',
       hidden: ({document}) => !document?.isMarkDown,
 
-    }
+    }, 
+    {
+      name: 'seo',
+      title: 'SEO',
+      type: 'seo-tools', // use seo-tools type
+      options: {
+          baseUrl: 'https://sanity-sapper-blog-web-oz3im2j8.netlify.app/', // (REQUIRED) This is the baseUrl for your site
+          baseUrl(doc) {
+              return 'https://sanity-sapper-blog-web-oz3im2j8.netlify.app/blog/'; // for dynamic baseUrls
+          },
+          slug(doc) { // (REQUIRED) a function to return the sug of the current page, which will be appended to the baseUrl
+              return doc.slug.current;
+          },
+          fetchRemote: true, // Can be set to false to disable fetching the remote source (you will need to pass the content helpers for analysis)
+          content(doc) {
+              return 'simple html representation of your doc'; // (OPTIONAL) If your site is generated after Sanity content updates you can use this for better real time feedback
+          },
+          title(doc) {
+              return 'page title'; // (OPTIONAL) return page title otherwise inferred from scrape
+          },
+          description(doc) {
+              return 'page description'; // (OPTIONAL) return page description otherwise inferred from scrape
+          },
+          locale(doc) {
+              return 'page locale'; // (OPTIONAL) return page locale otherwise inferred from scrape
+          },
+          contentSelector: 'body' // (OPTIONAL) option to finetune where Yoast will look for the content. (only applicable for scraping without content function)
+      }
+    },
   ],
   orderings: [
     {
@@ -109,7 +137,8 @@ export default {
         {
           field: 'title',
           direction: 'asc'
-        }
+        },
+       
       ]
     }
   ],
